2361f2d658add5ba71ebc6def059e82bbb164419
opilane <opilane@HP-CZC6297B8C>
2021-11-24 12:25:06 +0200

    .

--- a.py
+++ a.py
@@ -2,21 +2,19 @@ from module import*
 users=["Liza"]
 passwords=["132465"]
 
-password=passautomat()
-print(psword)
 while True:
     print("Reg-1, Avt-2, Välja-3")
     v=int(input())
-if v==0:
-    from module import*
-if v==1:
-    print("Registreerimine")
-    register(users,passwords)
-elif v==2:
-    print("Autoriseerimine")
+    if v==0:
+        from module import*
+    elif v==1:
+        print("Registreerimine")
+        register(users,passwords)
+    elif v==2:
+            print("Autoriseerimine")
 #Autoriseerimine teeme
-elif v==3:
-    print("Välja")
+    elif v==3:
+        print("Välja")
 #valmis
-else:
-    print("On vaja valida 1,2 või 3")#kõik on olemas
+    else:
+        print("On vaja valida 1,2 või 3")#kõik on olemas
---
+++ toofailidega.py
@@ -0,0 +1,38 @@
+def loe_failist(file:str)->str:
+    """Loeme tekst failist
+    """
+    f=open(file,"r") #f=file
+    stroka=f.readlines() #str
+    f.close()
+    return stroka #list
+stroka=loe_failist("TextFile1.txt")
+print(stroka)
+def loe_failist_listisse(file:str)->list:
+    """Loeme tekst failist ja salvesta järjendisse
+    """
+    f=open(file,"r")
+    list_=[]
+    for str in f:
+        list_.append(str.strip()) #strip-поиск символа и удаление его, append-добавление к списку
+        f.close()
+        return list_
+spisok=loe_failist_listisse("TextFile1.txt")
+print(spisok)
+
+def salvesta_failisse(file:str):
+    """Salvesta text failisse
+    """
+    f=open(file,"a")
+    text=input("Sisesta text:")
+    f.write(text+"\n")
+    f.close()
+
+for i in range(10):    #повторение
+    salvesta_failisse("Loetelu.txt")
+
+def faili_sisu_umberkirjutamine(file:str):
+    text=input("Sisesta uus text:")
+    with open(file,""w) as f:
+        f.write(text+"\n")
+
+faili_sisu_umberkirjutamine(input("Faili nimetus")+".txt)
